5cfca2c6dbfc6beb92a40332e1bdf44adf664eb1
julestrada <136045227+julestrada@users.noreply.github.com>
2023-08-17 20:44:42 -0700

    hw

--- Starter_Code/data/sql-challenge.sql
+++ Starter_Code/data/sql-challenge.sql
@@ -1,13 +1,13 @@
 drop table departments ;
 CREATE TABLE departments (
-    dept_no VARCHAR(225),
+    dept_no VARCHAR(225) PK,
     dept_name VARCHAR(255)
 );
 
-select * from departments ; 
+select * from departments limit 5 ; 
 
 CREATE TABLE title (
-	title_id VARCHAR (30),
+	title_id VARCHAR (30) PK,
 	tite VARCHAR (30)
 	
 	
@@ -16,8 +16,8 @@ CREATE TABLE title (
 	select * from title ;
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR (30),
-	emp_no VARCHAR (30)
+	dept_no VARCHAR (30) FK >- departments.dept_no,
+	emp_no VARCHAR (30)  FK >- employees.emp_no
 	
 	
 	
@@ -25,21 +25,21 @@ CREATE TABLE dept_manager (
 	select * from dept_manager ;	
 	
 CREATE TABLE dept_emp (
-	emp_no INT, 
-	dept_no VARCHAR (30)
+	emp_no INT FK>- employees.emp_no, 
+	dept_no VARCHAR (30) NULL FK >- departments.dept_no
 	);
 	select * from dept_emp ;
 	
 CREATE TABLE salaries ( 
-emp_no INT, 
+emp_no INT FK >-employees.emp_no, 
 salary INT
 
 );
 	select * from salaries ;
 	
 CREATE TABLE employees (
-	emp_no_ INT,
-	emp_title_id VARCHAR (30),
+	emp_no_ INT PK,
+	emp_title_id VARCHAR (30) FK >-titles.title_id,
 	birth_date VARCHAR (30),
 	first_name VARCHAR (30),
 	last_name VARCHAR (30),
@@ -52,6 +52,3 @@ CREATE TABLE employees (
 	SELECT first_name, last_name, hire_date
 FROM employees
 WHERE EXTRACT(YEAR FROM TO_DATE(hire_date, 'MM/DD/YYYY')) = 1986;
-
-
-
